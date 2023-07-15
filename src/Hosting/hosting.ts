dedf6f6853b2c5ff35f0469624a6689ad0dcf556
Alex-from-Ukraine <alex.zolotukhin@gigradar.io>
2023-07-15 09:49:13 +0300

    feat: create structure of hosting class

---
+++ src/Hosting/hosting.ts
@@ -0,0 +1,10 @@
+import { NewAccountCreator } from './NewAccountCreator';
+
+/**
+ * Main class for hosting functionality
+ */
+export class Hosting {
+  public createNewAccount() {
+    const newAccountCreator = new NewAccountCreator();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { Hosting } from './Hosting';
+
+/**
+ * Popup after successful registration
+ * <div class="top right ui toast-container  "></div>
+ */
+
+const hosting = new Hosting();
+hosting.createNewAccount();
