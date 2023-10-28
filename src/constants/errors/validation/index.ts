016b956cd810476858d0fcec7c2622c58b9f9579
Oleksii-KHDev <alexeyzolotuhin@gmail.com>
2023-10-28 09:51:51 +0300

    fix: tests

--- src/constants/errors/validation/index.ts
+++ src/constants/errors/validation/index.ts
@@ -4,7 +4,7 @@ export const ERROR_HOSTING_CREATION_OPTIONS = 1007;
 export const INVALID_HOSTING_PARAMETERS_ERROR: HttpErrorDetailsArray = [
   400,
   'invalid hosting parameters',
-  { code: ERROR_HOSTING_CREATION_OPTIONS },
+  { errorCode: ERROR_HOSTING_CREATION_OPTIONS },
 ];
 
 /**
@@ -14,5 +14,5 @@ export const ERROR_HOSTING_ACCOUNT_REGISTRATION_OPTIONS = 1008;
 export const INVALID_HOSTING_ACCOUNT_REGISTRATION_PARAMETERS_ERROR: HttpErrorDetailsArray = [
   400,
   'invalid parameters for registration new hosting account',
-  { code: ERROR_HOSTING_ACCOUNT_REGISTRATION_OPTIONS },
+  { errorCode: ERROR_HOSTING_ACCOUNT_REGISTRATION_OPTIONS },
 ];
